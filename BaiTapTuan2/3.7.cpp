29e5e5662551b750e8d32a7212fb7d58b7c19957
Thu Giang Hoang <78576738+hthzang@users.noreply.github.com>
2021-02-26 07:31:52 +0700

    Add files via upload

---
+++ BaiTapTuan2/3.7.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+ 
+
+int main()
+{
+	int a,b,c;
+	 cin>>a>>b;
+	      
+		   while(a!=b){a>b?c=a-b:c=b-a;}
+       a*b==0?cout<<a+b:cout <<c;
+	return 0;
+	}
---
+++ BaiTapTuan2/3.9.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	double a,b,c;
+	cin>>a>>b>>c;
+	a>c,a>b?cout<<a:(b>a,b>c?cout<<b:cout<<c);
+	
+	
+	
+	
+	
+}
