6adf358cc26de01eae185ea915521967414d4c28
Thu Giang Hoang <78576738+hthzang@users.noreply.github.com>
2021-02-26 22:34:48 +0700

    Add files via upload

--- BaiTapTuan2/BCNN.cpp
+++ BaiTapTuan2/BCNN.cpp
@@ -3,11 +3,13 @@ using namespace std;
 int main()
 {
 	int a, b;
-	int x = max(a,b) ;
+	cin >> a >> b; 
+	int x = max (a,b) ;
 	for (int i=x; i<=a*b;i++) {
 		if (i%a==0 && i%b==0) {
-		cout << "Boi chung nho nhat la: " << i << endl; 
+		cout << "Boi chung nho nhat la: " << i << endl;
+		break; 
 	} 
-}
+} 
 	return 0; 
  } 
