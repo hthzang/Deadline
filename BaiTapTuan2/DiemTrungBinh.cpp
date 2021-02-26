8aac615f3930df4e58caff98a3d42f0fa6e2fbc0
Thu Giang Hoang <78576738+hthzang@users.noreply.github.com>
2021-02-26 22:35:33 +0700

    Delete BaiTapTuan2 directory

--- BaiTapTuan2/DiemTrungBinh.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-using namespace std;
-int main()
-{
-	double mon1, mon2, mon3, mon4, mon5;
-	cin >> mon1 >> mon2 >> mon3 >> mon4 >> mon5;
-	cout << (mon1 + mon2 + mon3 + mon4 + mon5)/5; 
-	return 0;  
- } 
